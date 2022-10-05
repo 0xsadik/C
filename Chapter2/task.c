2d71edcfec5c748478dd9ef7e25bd2d10cdd98fc
0xsadik <torikussadik404@gmail.com>
2022-10-06 00:09:23 +0600

    task

--- Chapter2/task.c
+++ Chapter2/task.c
@@ -21,26 +21,26 @@
 
 // --> with double type 
 
-// #include<stdio.h>
-// int main()
-// {
-//     double num1, num2, sum;
-//     printf("enter first number:");
-//     scanf("%lf", &num1);
-//     printf("enter second number:");
-//     scanf("%lf", &num2);
+#include<stdio.h>
+int main()
+{
+    double num1, num2, sum;
+    printf("enter first number:");
+    scanf("%lf", &num1);
+    printf("enter second number:");
+    scanf("%lf", &num2);
 
-//     sum = num1 + num2;
-//     printf("addition is: %lf\n", sum);
+    sum = num1 + num2;
+    printf("addition is: %lf\n", sum);
 
-//     sum = num1 - num2;
-//     printf("Subtraction is: %lf\n", sum);
+    sum = num1 - num2;
+    printf("Subtraction is: %lf\n", sum);
 
-//     sum = num1 * num2;
-//     printf("multiplication is: %lf\n", sum);
+    sum = num1 * num2;
+    printf("multiplication is: %lf\n", sum);
 
-//     return 0;
-// }
+    return 0;
+}
 
 
 
