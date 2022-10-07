4b5483049998e70f8dfb9082b5026658a26ba448
0xsadik <torikussadik404@gmail.com>
2022-10-07 22:34:32 +0600

    condition

--- Chapter3/condition_base.c
+++ Chapter3/condition_base.c
@@ -1,6 +1,42 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int n = 10;
+//     if (n < 0) {
+//         printf("The number is negative.\n");
+//     }
+//     else {
+//         printf("The number is positive\n");
+//     }
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     int n = 11;
+//     if (n < 0) {
+//         printf("The number is negative.\n");
+//     }
+//     else if ( n > 0) {
+//         printf("The number is poitive\n");
+//     }
+//     else {
+//         printf("The number is zero.\n");
+//     }
+
+//     return 0;
+// }
+
+
 #include<stdio.h>
 int main()
 {
+    int n = 12;
 
+    if ( n > 10) {
+        printf("The number is greater than 10\n");
+    }
     return 0;
 }
