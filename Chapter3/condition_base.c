22fa09f4fa8056b7f46607466f09523d5656b92a
0xsadik <torikussadik404@gmail.com>
2022-10-08 07:56:03 +0600

    without mod operator

--- Chapter3/condition_base.c
+++ Chapter3/condition_base.c
@@ -56,19 +56,74 @@
 // }
 
 
+// #include<stdio.h>
+// int main()
+// {
+//     int num = 4, remainder;
+//     remainder = num % 2;
+//     if (remainder == 0) {
+//         printf("The number is even\n");
+//     }
+//     else {
+//         printf("The number is odd\n");
+//     }
+
+//     return 0;
+// }
+
+
+// --> without remainder varibale 
+
+// #include<stdio.h>
+// int main()
+// {
+//     int num;
+//     printf("Enter a number to check even or odd: ");
+//     scanf("%d", &num);
+//     if (num % 2 == 0) {
+//         printf("The number is even\n");
+//     }
+//     else {
+//         printf("The number is odd\n");
+//     }
+
+//     return 0;
+// }
+
+
+// --> without mod operator 
+
+// #include<stdio.h>
+// int main()
+// {
+//     int num, vagfol, vagsesh;
+
+//     printf("Enter a number: ");
+//     scanf("%d", &num);
+
+//     // vagfol = num / 2;
+//     // vagsesh = num - (2 * vagfol);
+//     vagsesh = num - (2 * (num / 2 ));
+
+//     printf("vagsesh = %d\n", vagsesh);
+
+//     return 0;
+
+// }
+
+
 #include<stdio.h>
 int main()
 {
-    int num = 4, remainder;
-    remainder = num % 2;
-    if (remainder == 0) {
-        printf("The number is even\n");
+    int num = 7, vagsesh;
+    vagsesh = num - (2 * (num / 2));
+    if (vagsesh == 0) {
+        printf("The number is even.\n");
     }
     else {
-        printf("The number is odd\n");
+        printf("The number is odd.\n");
     }
 
     return 0;
 }
-
 
