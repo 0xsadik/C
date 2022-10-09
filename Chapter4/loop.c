e12f355e3aacc4c12fc58d80c6eb1e78e0a6fbd5
0xsadik <torikussadik404@gmail.com>
2022-10-09 18:54:55 +0600

    loop

---
+++ Chapter4/loop.c
@@ -0,0 +1,65 @@
+
+// while 
+
+// #include<stdio.h>
+// int main()
+// {
+//     int n = 1;
+
+//     while (n <= 10) {
+//         printf("%d\n", n);
+//         n++;
+//     }
+
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     int n = 1;
+//     while (n <= 10) {
+//         printf("%d\n", n);
+//     }
+//     n++;
+
+//     return 0;
+// }
+
+
+// break statement 
+
+// #include<stdio.h>
+// int main()
+// {
+//     int n = 1;
+//     while ( n <= 100) {
+//         printf("%d\n", n);
+//         n++;
+
+//         if(n > 10) {
+//             break;
+//         }
+//     }
+
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     int n = 0;
+//     while ( n < 10) {
+//         n = n + 1;
+//         if ( n % 2 == 0) {
+//             continue;
+//         }
+
+//         printf("%d\n", n);
+//     }
+//     return 0;
+// }
+
+
