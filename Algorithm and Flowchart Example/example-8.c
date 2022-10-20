269c9f422ceb205bc6c5bfb0ea248c0647c744a1
0xsadik <torikussadik404@gmail.com>
2022-10-20 20:49:12 +0600

    example - 8

---
+++ Algorithm and Flowchart Example/example-8.c
@@ -0,0 +1,13 @@
+// একটি বৃত্তের ক্ষেত্রফল নির্ণয়
+
+#include<stdio.h>
+int main()
+{
+    float r, area;
+    scanf("%f", &r);
+
+    area = 3.16 * r * r;
+    printf("Area of circle: %2.f\n", area);
+
+    return 0;
+}
