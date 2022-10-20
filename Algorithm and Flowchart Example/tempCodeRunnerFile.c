74a59eace99fa087c6e180a5b77f627f1b36913d
0xsadik <torikussadik404@gmail.com>
2022-10-20 20:49:48 +0600

    dele

--- Algorithm and Flowchart Example/tempCodeRunnerFile.c
+++
@@ -1,9 +0,0 @@
-int a, b, c, s, area;
-
-    scanf("%d", &a);
-    scanf("%d", &b);
-    scanf("%d", &c);
-
-    s = (a + b + c) / 2;
-    area = sqrt(s*(s-a)*(s-b)*(s-c));
-    printf("Area of Triangle: %d\n", area);
