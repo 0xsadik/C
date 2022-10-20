62c804d3cd83ad551015f431653330f86f29d83d
0xsadik <torikussadik404@gmail.com>
2022-10-20 20:13:15 +0600

    example - 6

---
+++ Algorithm and Flowchart Example/example-6.c
@@ -0,0 +1,14 @@
+// একটি ত্রিভুজের ক্ষেত্রফল নির্নয় 
+
+#include<stdio.h>
+int main()
+{
+    int b, h, area;
+    scanf("%d", &b);
+    scanf("%d", &h);
+
+    area = .5 * b * h;
+    printf("Area is: %d\n", area);
+
+    return 0;
+}
