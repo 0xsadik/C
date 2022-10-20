ba12b30d0f43486d800297e1fbd97009d678ab49
0xsadik <torikussadik404@gmail.com>
2022-10-20 20:05:23 +0600

    example - 4

---
+++ Algorithm and Flowchart Example/example-4.c
@@ -0,0 +1,15 @@
+ // তিনটি সংখ্যার যোগফল নির্নয়ের এলগরিদম ও ফ্লো চার্ট
+
+ #include<stdio.h>
+ int main()
+ {
+    int a, b, c, sum;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+
+    sum = a + b + c;
+    printf("sum is: %d\n", sum);
+
+    return 0;
+ }
