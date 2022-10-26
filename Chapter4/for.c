6398a6967e2e21051c8f6f1a17cdac0de2618bc0
0xsadik <torikussadik404@gmail.com>
2022-10-26 19:12:16 +0600

    loop

--- Chapter4/for.c
+++ Chapter4/for.c
@@ -125,17 +125,37 @@
 // }
 
 
+// #include<stdio.h>
+// int main()
+// {
+//     int a, b, c;
+
+//     for ( a = 1; a <= 3; a++ ){
+//         for (b = 1; b <= 3 && b != a; b++) {
+//             for (c = 1; c <= 3 && c != a && c != b; c++) {
+//                 printf("%d, %d, %d\n", a, b, c);
+//             }
+//         }
+//     }
+// }
+
+
+
 #include<stdio.h>
 int main()
 {
     int a, b, c;
 
-    for ( a = 1; a <= 3; a++ ){
-        for (b = 1; b <= 3 && b != a; b++) {
-            for (c = 1; c <= 3 && c != a && c != b; c++) {
-                printf("%d, %d, %d\n", a, b, c);
+    for (a = 1; a <= 3; a++) {
+        for (b = 1; b <= 3; b++) {
+            if ( b != a ) {
+                for (c = 1; c <= 3; c++) {
+                    if ( c != b && c != a ) {
+                        printf("%d, %d, %d\n", a, b, c);
+                    }
+                }
             }
-            printf("\n");
         }
     }
+    return 0;
 }
