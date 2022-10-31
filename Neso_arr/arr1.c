44a83f82a67ffe0fe6276f60915b18fbccd1b8d2
0xsadik <torikussadik404@gmail.com>
2022-10-31 15:56:25 +0600

    macro

--- Neso_arr/arr1.c
+++ Neso_arr/arr1.c
@@ -1,2 +1,19 @@
 
-skdjf
+#include<stdio.h>
+#define N 10 // whic is macro i guess
+
+int main()
+{
+    int a[N], i;
+    for (i = 0; i < N; i++) {
+        printf("Enter the input for index %d: ", i);
+        scanf("%d", &a[i]);
+    }
+
+    printf("\nArary elements are as follows:\n");
+    for (i = 0; i < N; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
