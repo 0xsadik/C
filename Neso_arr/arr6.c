95d9c2c17a4845a166a61f28cbb25a5a958a6cd8
0xsadik <torikussadik404@gmail.com>
2022-11-03 07:23:41 +0600

    function

---
+++ Neso_arr/arr6.c
@@ -0,0 +1,28 @@
+
+// ====== Definition and syntax ======
+
+// multidimentional arrays can be defined as an array of arrays 
+
+// general form of declaring N-dimensional array is as follows:
+
+// data_type name_of_array[size1][size2]...[sizeN]
+
+// for example 
+
+// int a[3][4];     --> Two dimentional array
+// int a[3][4][6]   --> Three dimensional array
+
+ 
+ // ---- size calculation ----
+
+ // size of multidimentional array can be calculated by multiplyng the size of all the dimensions
+
+// For example: 
+
+//     size of a[10][20] = 10 x 20  = 200;
+//                       = 200 x 4  = 800 bytes
+
+//     size of a[4][10][20] = 4 x 10 x 20 = 800;
+//                          = 800 x 4 = 3200 bytes 
+
+
