2b5f6bb0ff8b2ea807ce2772301a0fcc3c93b7c0
0xsadik <torikussadik404@gmail.com>
2023-06-10 16:12:09 +0600

    integer type

---
+++ 1.DataTypes/1.dataTypes.c
@@ -0,0 +1,83 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int a;
+//     int b;
+//     int sum;
+//     a = 50;
+//     b = 60;
+//     sum = a + b;
+//     printf("Sum is %d\n", sum);
+
+//     return 0;
+// }
+
+
+// --> another way 
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a, b, sum;
+//     a = 50;
+//     b = 60;
+//     sum = a + b;
+//     printf("Sum is: %d\n", sum);
+
+//     return 0;
+// }
+
+
+// --> declaration and assign both 
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a = 50, b = 60, sum;
+//     sum = a + b;
+//     printf("Sum is: %d\n", sum);
+
+//     return 0;
+// }
+
+
+// --> Question
+
+// #include<stdio.h>
+// int main()
+// {
+//     int x, y;
+//     x = 1;
+//     y = x;
+//     x = 2;
+
+//     printf("%d\n", y);
+
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a = 50, b = 60, sum;
+//     sum = a + b;
+//     printf("%d + %d = %d\n", a, b, sum);
+
+//     return 0;
+// }
+
+
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a = 50.34, b = 60, sum;
+//     sum = a + b;
+//     printf("%d + %d = %d\n", a, b, sum);
+
+//     return 0;
+// }
+
+
