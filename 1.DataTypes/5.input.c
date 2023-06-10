e3c17ca6b585e87f3580839d98fb22af48b5db02
0xsadik <torikussadik404@gmail.com>
2023-06-10 16:31:53 +0600

    input

---
+++ 1.DataTypes/5.input.c
@@ -0,0 +1,14 @@
+// input from user 
+
+#include<stdio.h>
+int main()
+{
+    int a, b, sum;
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    sum = a + b;
+    printf("Sum is: %d\n", sum);
+
+    return 0;
+}
